7743c9e945055f9dda7ffdbdb5e5e33eaf72e352
spicy-films <35748743+spicy-films@users.noreply.github.com>
2018-04-10 19:50:36 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// google jquery ready function 
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+        
+ });
